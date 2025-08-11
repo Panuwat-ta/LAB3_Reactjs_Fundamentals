414c93bc4f6c94f679f2569b94ddd8023a70cbff
panuwat-ta <panuwattakham2002@gmail.com>
2025-08-11 22:54:13 +0700

    Challenge 3: แก้ปัญหา Prop Drilling ด้วย useContext Hook

--- experiment2/react-workshop/src/component/TodoList.jsx
+++ experiment2/react-workshop/src/component/TodoList.jsx
@@ -1,18 +1,15 @@
 // src/TodoList.jsx
-import React from 'react';
-import TodoItem from './TodoItem'; // 🔽 Import
+import React, { useContext } from 'react';
+import TodoItem from './TodoItem';
+import { TodoContext } from '../contexts/TodoContext';
 
-function TodoList({ todos, deleteTodo, toggleTodo, editTodo }) { // 🔽 รับ editTodo
+function TodoList() { // 🔽 ไม่ต้องรับ props แล้ว
+  const { todos } = useContext(TodoContext); // 🔽 ดึง state มาจาก context
+
   return (
     <ul className="todo-list">
       {todos.map(todo => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          deleteTodo={deleteTodo}
-          toggleTodo={toggleTodo}
-          editTodo={editTodo} // 🔽 ส่งต่อ editTodo
-        />
+        <TodoItem key={todo.id} todo={todo} />
       ))}
     </ul>
   );
